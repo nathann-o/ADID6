c89c7a5261a2834535668362b4f0d6841f5adbc7
Nathann <nathann_oliveira@hotmail.com.br>
2021-04-12 22:49:33 -0300

    Primeiro envio

---
+++ src/main/java/br/edu/ifsp/ADID6/Adid6Application.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.ADID6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adid6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adid6Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/ADID6/Adid6ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.ADID6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Adid6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
